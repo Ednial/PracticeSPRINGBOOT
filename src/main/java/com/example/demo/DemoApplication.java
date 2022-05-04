a46e6ae53e34747e98060f392b819befb61ee181
Ednial <ericxonb@gmail.com>
2022-05-04 12:51:35 -0500

    "Implements StudentClass as a JsonFile"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,8 +18,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello","world");
+	public List<Student> hello(){
+		return List.of(
+				new Student(1L,"Ericxon","Ericxonb@gmail.com", LocalDate.of(1998, Month.MARCH,03),24)
+		);
 	}
 
 }
